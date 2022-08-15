205b0124724c5c29ce128023bed4a56a0dbf4739
Asmae DINO <67391075+asmae-din@users.noreply.github.com>
2022-08-15 21:14:47 +0100

    up 15/08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,32 @@ export class AppComponent implements OnInit{
   mySnap!:FaceSnap;
   mySecSnap!:FaceSnap;
   myLastSnap!:FaceSnap;
+
   ngOnInit(){
-    this.mySnap=new FaceSnap(
-      "ASMAE",
-      "20yo female",
-      "https://images.pexels.com/photos/13020454/pexels-photo-13020454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      new Date,
-      0,
-      'Casablanca'
-      
-    );
-    this.mySecSnap=new FaceSnap(
-      "ASMAE",
-      "25yo female",
-      "https://images.pexels.com/photos/12712600/pexels-photo-12712600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      new Date,
-      0
-    );
-    this.myLastSnap=new FaceSnap( 
-      "ASMAE",
-      "32yo female",
-      "https://images.pexels.com/photos/12789556/pexels-photo-12789556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      new Date,
-      0
-    );
+    this.mySnap={
+    title:"ASMAE",
+    description:"20yo female",
+    imageUrl:"https://images.pexels.com/photos/13020454/pexels-photo-13020454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate:new Date(),
+    snaps:0,
+    location:'Casablanca'
+    };
+
+    this.mySecSnap={
+      title:"ASMAE",
+      description:"25yo female",
+      imageUrl:"https://images.pexels.com/photos/12712600/pexels-photo-12712600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      createdDate:new Date(),
+      snaps:0,
+      location:'L.A'
+    };
+    this.myLastSnap={
+      title:"ASMAE",
+      description:"32yo female",
+      imageUrl:"https://images.pexels.com/photos/12789556/pexels-photo-12789556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      createdDate:new Date(),
+      snaps:0
+    };
   }
  
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,12 +1,10 @@
 export class FaceSnap{
 
-     constructor(
-        public title:string, 
-        public description:string, 
-        public imageUrl:string, 
-        public createdDate:Date,
-        public snaps:number,
-        public location?:string){
+         title!:string;
+         description!:string;
+         imageUrl!:string;
+         createdDate!:Date;
+         snaps!:number;
+         location?:string;
+      }
  
-     }
-}
