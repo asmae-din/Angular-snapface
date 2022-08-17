7621808708278f74312237ca0a4fc4ac83ddcea1
asmae din <assouma4ever@gmail.com>
2022-08-17 15:57:19 +0100

    up 17/08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.faceSnapsArr=[{
-      title:"ASMAE",
+      title:"asmae",
       description:"20yo female",
       imageUrl:"https://images.pexels.com/photos/13020454/pexels-photo-13020454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       createdDate:new Date(),
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit{
       location:'Casablanca'
       },
       {
-        title:"ASMAE",
+        title:"the cave",
         description:"25yo female",
         imageUrl:"https://images.pexels.com/photos/12712600/pexels-photo-12712600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         createdDate:new Date(),
@@ -28,13 +28,13 @@ export class AppComponent implements OnInit{
         location:'L.A'
       },
       {
-        title:"ASMAE",
+        title:"beauty under the moon",
         description:"32yo female",
         imageUrl:"https://images.pexels.com/photos/12789556/pexels-photo-12789556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         createdDate:new Date(),
         snaps:0
       }]
   }
- 
+  
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule , LOCALE_ID} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
-
+import * as fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent
+    FaceSnapComponent,
+    FaceSnapListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    registerLocaleData(fr.default);
+  }
+}
